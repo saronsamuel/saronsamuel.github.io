028e4572c1f8bc4ced12222e24bcf6cb8d212c4e
saronsamuel <sdsam@stanford.edu>
2024-08-22 21:05:47 -0400

    updating info

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,17 +1,26 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Stanford, California",
+    37.422590,
+    -122.165413
   ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
+
   [
     "Los Angeles, CA",
     34.0543942,
     -118.2439408
+  ],
+
+  [
+    "Portland, OR",
+    45.523064,
+    -122.676483
   ]
+
+  [
+    "Baltimore, MD",
+    39.299236,
+    -76.609383
+  ]
+
 ];
