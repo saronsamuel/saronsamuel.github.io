e0d1209592c94b7604fdf743f7cab6f299eacf48
saronsamuel <68874516+saronsamuel@users.noreply.github.com>
2025-03-10 20:58:52 +0000

    Deploying to gh-pages from @ saronsamuel/saronsamuel.github.io@ce8acf45f67d38856cd8cb88d34e58091675932a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-",
-          title: "",
+  },{id: "nav-cv",
+          title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
