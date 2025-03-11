e1c68d2d2a0e6f18eb6dd4be72efe369a449b6e1
saronsamuel <68874516+saronsamuel@users.noreply.github.com>
2025-03-11 04:22:44 +0000

    Deploying to gh-pages from @ saronsamuel/saronsamuel.github.io@b77d71beeada0efa8f827ce1ad695b10756926a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/_pages/cv/";
           },
         },{id: "post-a-post-with-image-galleries",
       
