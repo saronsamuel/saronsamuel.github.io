09fa72a2d8179f00919f66e7ba28df6d70cb662f
saronsamuel <68874516+saronsamuel@users.noreply.github.com>
2025-03-11 05:36:30 +0000

    Deploying to gh-pages from @ saronsamuel/saronsamuel.github.io@1ed7b8a5c67fceecb953f82831ed88f836ad155b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -373,16 +373,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-ll-be-attending-johns-hopkins-for-my-cs-phd-honored-to-receive-the-percy-pierre-doctoral-fellowship",
+          title: 'I’ll be attending Johns Hopkins for my CS PhD, honored to receive the...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
